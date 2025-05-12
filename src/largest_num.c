1ce87803801cd276cae8f6bafc75723587444a71
Bella Bernal <bella.bernal@atlasschool.com>
2025-05-12 18:31:53 +0000

    student facing changes - checks will now fail

--- src/largest_num.c
+++ src/largest_num.c
@@ -2,19 +2,5 @@
 
 
 int find_max(int num1, int num2, int num3) {
-    int max;
-    if (num1 >= num2) {
-        if (num1 >= num3) {
-            max = num1;
-        } else {
-            max = num3;
-        }
-    } else {
-        if (num2 >= num3) {
-            max = num2;
-        } else {
-            max = num3;
-        }
-    }
-    return max;
+    /* Code here */
 }
--- src/main.c
+++ src/main.c
@@ -2,11 +2,5 @@
 #include <stdio.h>
 
 int main() {
-    int num1, num2, num3;
-    printf("Enter three numbers: ");
-    scanf("%d %d %d", &num1, &num2, &num3);
-
-    int max = find_max(num1, num2, num3);
-    printf("The largest number is: %d\n", max);
-    return 0;
+     /* Code here */
 }
