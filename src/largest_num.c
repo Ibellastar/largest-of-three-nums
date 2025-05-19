7996bfe4bc13e2ee3e016c007db53d821b5ea339
Bella Bernal <bella.bernal@atlasschool.com>
2025-05-19 17:23:36 +0000

    Student facing changes

--- src/largest_num.c
+++ src/largest_num.c
@@ -1,19 +1,5 @@
 #include <stdio.h>
 
 int find_max(int num1, int num2, int num3) {
-    // int max;
-    // if (num1 >= num2) {
-    //     if (num1 >= num3) {
-    //         max = num1;
-    //     } else {
-    //         max = num3;
-    //     }
-    // } else {
-    //     if (num2 >= num3) {
-    //         max = num2;
-    //     } else {
-    //         max = num3;
-    //     }
-    // }
-    // return max;
+   /* Your code here */
 }
